f9b7892718a41b65131fe9b73071fd806dd91bf5
QuocPhiVong <101709348+QuocPhiVong@users.noreply.github.com>
2024-09-17 20:38:54 +0700

    CM02T-SE161993-OnBoardingScreenUI
    feat: onboarding screen (after splash screen before login screen)

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -7,4 +7,9 @@ class TImages{
 
   //sign in option logo
   static String google = "assets/logos/google.png";
+
+  //on boarding images
+  static String onBoardingImage1 = "assets/images/on_boarding_images/sammy-line-searching.gif";
+  static String onBoardingImage2 = "assets/images/on_boarding_images/sammy-line-shopping.gif";
+  static String onBoardingImage3 = "assets/images/on_boarding_images/sammy-line-delivery.gif";
 }
